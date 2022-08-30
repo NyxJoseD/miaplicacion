9fce738312945054385cfdb13f9742a4a3fa6543
NyxJoseD <josedanielmelean@gmail.com>
2022-08-30 22:26:32 +0100

    cambios2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ContactoAComponents from './components/pure/contactoA';
 import Ejemplo1 from './hooks/Ejemplo1';
 import Ejemplo2 from './hooks/Ejemplo2';
 import Ejemplo3 from './hooks/Ejemplo3';
+import Ejemplo4 from './hooks/Ejemplo4';
 
 function App() {
   return (
@@ -21,6 +22,11 @@ function App() {
         {/* Ejemplo de uso de HOOKS */}
         <Ejemplo1></Ejemplo1>
         <miComponenteConContexto></miComponenteConContexto>
+        <Ejemplo4 nombre="Martin"></Ejemplo4>
+        {/* Todo lo que hay aqui adentro es tratado como props.children */}
+        <h3>
+          Contenido del props.children
+        </h3>
       </header>
     </div>
   );
